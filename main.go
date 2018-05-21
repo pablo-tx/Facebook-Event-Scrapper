2f09fb6fc9016bc2b39656e29fe6c792da69223c
Pablo <ptx@protonmail.com>
2018-05-22 01:25:14 +0200

    Add executable to gitignore

--- main.go
+++ main.go
@@ -6,25 +6,8 @@ import (
 	"sort"
 	"strconv"
 
-	gq "github.com/PuerkitoBio/goquery"
 	fb "github.com/huandu/facebook"
 )
-
-// ParseEvents ...
-func ParseEvents(url string) (string, error) {
-	doc, err := gq.NewDocument(url)
-	fmt.Println(doc.Html())
-	if err != nil {
-		return "", err
-	}
-
-	s := doc.Find("data_hovercard").First()
-	cmt, err := s.Html()
-	if err != nil {
-		return "", err
-	}
-	return cmt, nil
-}
 
 func main() {
 
@@ -33,12 +16,9 @@ func main() {
 		name      string
 		link      string
 		likecount int64
-		//rating    float64
 	}
 
 	var center = "37.176487,-3.597929"
-	//searchList := []string{"Party", "Discoteca", "Club", "Pub", "Nightclub"}
-	//for _, search := range searchList {
 	res, _ := fb.Get("/search", fb.Params{
 		"access_token": "529285107466818|bd7qQ2XQBSvJ3hRwdb7RPg6LTGY",
 		"type":         "place",
@@ -83,31 +63,8 @@ func main() {
 	})
 
 	for _, result := range results {
-		html, err := ParseEvents(result.link)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		println(html)
+
 		fmt.Println(*result)
-		/*res, _ := fb.Get("/"+strconv.Itoa(result.id)+"/events", fb.Params{
-			"access_token":       "529285107466818|bd7qQ2XQBSvJ3hRwdb7RPg6LTGY",
-			"include_canceled":   "false",
-			"time_filter":        "upcoming",
-			"event_state_filter": "published",
-		})
-		fmt.Println(res)
-		var eventitems []fb.Result
-		err := res.DecodeField("data", &eventitems)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		for _, item := range eventitems {
-			fmt.Println(*result, item)
-		}*/
 
 	}
 
